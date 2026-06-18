2959f9cd4c8a38cb047192e75eb107be4721eaa5
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2026-06-18 01:20:28 -0300

    cleanup

--- download_graph_local.py
+++
@@ -1,7 +0,0 @@
-
-import osmnx as ox
-
-_DEFAULT_PLACE = "São Paulo, SP, Brazil"
-_DEFAULT_NETWORK = "drive"
-
-ox.save_graphml(ox.graph_from_place(_DEFAULT_PLACE, network_type=_DEFAULT_NETWORK), "sp.graphml")
